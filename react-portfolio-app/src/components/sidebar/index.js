0c7757c94b4fbe8d60b5a4b055ce8a31a8aa7577
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-07 20:36:58 -0400

    added custom logo to sidebar index

--- react-portfolio-app/src/components/sidebar/index.js
+++ react-portfolio-app/src/components/sidebar/index.js
@@ -1,11 +1,14 @@
 import './index.scss';
 import {Link} from 'react-router-dom';
+import SubLogo from '../../assets/images/Sub-Logo.png';
+import MainLogo from '../../assets/images/Main-Logo.png';
 
 
 const Sidebar = () => (
     <div className='nav-bar'>
 <Link className='logo' to='/'>
-    <img src=
+    <img src={SubLogo} alt='logo'/>
+    <img src={MainLogo} alt='megan'/>
 </Link>
     </div>
 );
