39be1aa4ee3ad699f76615e7d753656b7e1978b7
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-07 22:06:19 -0400

    added a login to dashboard page

--- react-portfolio-app/src/components/dashboard/index.js
+++ react-portfolio-app/src/components/dashboard/index.js
@@ -4,3 +4,26 @@ import Home from "./home";
 import Login from '../Login';
 
 const Dashboard = () => {
+
+    const [user, setUser] = useState(null);
+    const auth = getAuth();
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+        })
+    }, []);
+
+
+    return (
+       <div>
+           {user ? <Home /> : <Login />}
+       </div>
+    )
+}
+
+export default Dashboard;
