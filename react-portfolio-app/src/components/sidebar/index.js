ab659af5ea0f833c791515ec9a372df7a970c98a
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-07 20:46:18 -0400

    set the rest of the imports for the sidebar index

--- react-portfolio-app/src/components/sidebar/index.js
+++ react-portfolio-app/src/components/sidebar/index.js
@@ -1,17 +1,40 @@
 import './index.scss';
-import {Link} from 'react-router-dom';
+import { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom';
 import SubLogo from '../../assets/images/Sub-Logo.png';
 import MainLogo from '../../assets/images/Main-Logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faLinkedin,
+    faGithub,
+    faYoutube,
+    faSkype,
+  } from '@fortawesome/free-brands-svg-icons'
+  import {
+    faHome,
+    faUser,
+    faEnvelope,
+    faSuitcase,
+    faBars,
+    faClose,
+  } from '@fortawesome/free-solid-svg-icons'
 
-
-const Sidebar = () => (
+const Sidebar = () => {
+    const [showNav, setShowNav] = useState(false);
+    
+    return (
     <div className='nav-bar'>
 <Link className='logo' to='/'>
     <img src={SubLogo} alt='logo'/>
     <img className='sub-logo' src={MainLogo} alt='megan'/>
 </Link>
+<nav>
+    <NavLink exact='true' activeclassname='active' to='/'>
+        <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>
+    </NavLink>
+</nav>
     </div>
-);
+)};
 
 
 export default Sidebar;
