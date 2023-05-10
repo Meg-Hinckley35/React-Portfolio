17fa1f35e2cdbaa12a42db7b11eb07416d8d1d52
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-10 14:14:01 -0400

    updating the layout, and reviewing firebase documentation

--- react-portfolio-app/src/components/about/index.js
+++ react-portfolio-app/src/components/about/index.js
@@ -8,7 +8,7 @@ import {
   faReact,
 } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
-import AnimatedLetters from '../AnimatedLetters'
+import AnimatedLetters from '../animated letters/index';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
 
